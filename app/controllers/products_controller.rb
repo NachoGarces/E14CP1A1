2be2f83f3c3e34b944781c3e05b50502189760cf
ignacio <nachogarces15@gmail.com>
2019-04-20 20:45:44 -0400

    first commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,18 @@
+class ProductsController < ApplicationController
+  def create
+    @product = Product.new(product_params)
+    @product.save
+    @category = @product.category
+    redirect_to @category
+  end
+
+  def destroy
+      @product.destroy
+      redirect_to @product.category
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :price, :category_id)
+  end
+end
